d9289c49f60825b22976801eab1c2abdd734f6c5
Trevor Hodsdon <mrtj458@gmail.com>
2019-12-22 15:07:45 -0700

    Added AWS S3 for production

--- sorfoundation/settings.py
+++ sorfoundation/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     # Packages
     'django_summernote',
+    'storages',
     'django_cleanup',  # Cleanup must be last
 ]
 
@@ -139,15 +140,22 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+# Summernote config
 X_FRAME_OPTIONS = 'SAMEORIGIN'
-
-SUMMERNOTE_CONFIG = {
-    'disable_attachment': True,
-}
-
 
 # Email
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
+
+# S3
+if not DEBUG:
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
